39012b614b49bd8b214bb1c7ae9e78832d7230e0
Sameer thakur <wabmetacontact@gmail.com>
2026-05-20 21:17:13 +0530

    Remove Keynotes and Business stats from Hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -50,14 +50,6 @@ const Hero = () => {
                         <span className="text-4xl font-black text-primary font-display">30+</span>
                         <span className="text-xs font-black uppercase tracking-widest opacity-60">Years Experience</span>
                     </div>
-                    <div className="flex flex-col">
-                        <span className="text-4xl font-black text-primary font-display">50+</span>
-                        <span className="text-xs font-black uppercase tracking-widest opacity-60">Keynotes Delivered</span>
-                    </div>
-                    <div className="flex flex-col">
-                        <span className="text-4xl font-black text-primary font-display">$100M+</span>
-                        <span className="text-xs font-black uppercase tracking-widest opacity-60">Business Built</span>
-                    </div>
                 </motion.div>
             </div>
         </section>
